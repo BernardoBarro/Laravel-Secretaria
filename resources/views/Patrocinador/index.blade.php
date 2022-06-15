7cdb445ac77fcba3bfebd8a01aef4c1512695a2b
Matheus <matheusgalvain@gmail.com>
2022-06-15 02:57:36 -0300

    Ajuste na mensagem

--- resources/views/Patrocinador/index.blade.php
+++ resources/views/Patrocinador/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     @foreach($patrocinadores as $patrocinador)
         <li>Nome do Patrocinador: {{ $patrocinador->nome }}</li>    
-        <li>Valor a contribuir: {{ $patrocinador->valor }}</li>
+        <li>Valor contribuído: R${{ $patrocinador->valor }}</li>
         <li>Descrição: {{ $patrocinador->descricao }}</li>
         <br>
     @endforeach    
