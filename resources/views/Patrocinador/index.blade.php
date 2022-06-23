064267d30a28ddccf8f7ea3326f0898e2aa2269a
Matheus <matheusgalvain@gmail.com>
2022-06-22 23:12:45 -0300

    listagem, edit, e destroy de patrocinador

--- resources/views/Patrocinador/index.blade.php
+++ resources/views/Patrocinador/index.blade.php
@@ -1,12 +1,34 @@
-@extends('adminlte::page')
+@extends('layouts.default')
 
 @section('content')
-    @foreach($patrocinadores as $patrocinador)
-        <li>Nome do Patrocinador: {{ $patrocinador->nome }}</li>    
-        <li>Valor contribuído: R${{ $patrocinador->valor }}</li>
-        <li>Descrição: {{ $patrocinador->descricao }}</li>
-        <br>
-    @endforeach    
+	<h1>Patrocinadores Registrados:</h1>
+
+	<table class="table table-stripe table-bordered table-hover">
+		<thead>
+			<th>Nome do Patrocinador:</th>
+			<th>Valor Doado:</th>
+            <th>Descrição:</th>
+            <th>Ações:</th>
+		</thead>
+
+		<tbody>
+			@foreach($patrocinadores as $patrocinador)
+				<tr>
+                    <td>{{ $patrocinador->nome }}</td>
+                    <td>{{ $patrocinador->valor }}</td>
+					<td>{{ $patrocinador->descricao }}</td>
+					<td>
+						<a href="{{ route('patrocinador.edit', ['id'=>\Crypt::encrypt($patrocinador->id)]) }}" class="btn-sm btn-success">Editar</a>
+						<a href="#" onclick="return ConfirmaExclusao({{$patrocinador->id}})"class="btn-sm btn-danger">Remover</a>
+					</td>
+				</tr>
+			@endforeach
+		</tbody>
+	</table>	
+
+	
+
+	<a href="{{ route('patrocinador.create', []) }}" class="btn btn-info">Adicionar</a>
 @stop
 
 @section('table-delete')
